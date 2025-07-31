35b4138b334c519f2bc4d472b2e7df13a9d3dada
Xinmei Xu <xu.xinmei2467@gmail.com>
2025-07-31 04:05:11 +0000

    update

--- moneychanger.py
+++ moneychanger.py
@@ -5,11 +5,21 @@ from dotenv import load_dotenv
 import requests as r 
 from datetime import datetime 
 from zoneinfo import ZoneInfo 
+import streamlit as st
 
 load_dotenv() # read .env file and add to my environment 
 EXCHANGERATE_API = os.getenv('EXCHANGERATE_API_KEY') # retrieve a variable's value from my current environment (os.environ)
 
 base_url = f"https://v6.exchangerate-api.com/v6/{EXCHANGERATE_API}/pair"
+
+# Create app layout 
+st.title("Multilingual Money Changer")
+user_input = st.text_area("Enter the amount of currency to change:")
+
+if st.button("Submit"):
+    st.write("**You entered:**")
+    st.write(user_input)
+
 
 # Convert UTC time to readable format in PST 
 def parse_time(utc_time):
